d71da80e9c55f3ed60df1abfc1c5c1bac12ae062
Abdullah Qayyum <i230049@isb.nu.edu.pk>
2025-07-06 01:34:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const posts = [
   {
-    name: "Vincent van Gogh",
+    name:     "Vincent van Gogh",
     username: "vincey1853",
     location: "Zundert, Netherlands",
-    avatar: "images/avatar-vangogh.jpg",
-    post: "images/post-vangogh.jpg",
-    comment: "just took a few mushrooms lol",
-    likes: 21
+    avatar:   "/images/avatar-vangogh.jpg",
+    post:     "/images/post-vangogh.jpg",
+    comment:  "just took a few mushrooms lol",
+    likes:    21
   },
   {
-    name: "Gustave Courbet",
+    name:     "Gustave Courbet",
     username: "gus1819",
     location: "Ornans, France",
-    avatar: "images/avatar-courbet.jpg",
-    post: "images/post-courbet.jpg",
-    comment: "i'm feelin a bit stressed tbh",
-    likes: 4
+    avatar:   "/images/avatar-courbet.jpg",
+    post:     "/images/post-courbet.jpg",
+    comment:  "i'm feelin a bit stressed tbh",
+    likes:    4
   },
   {
-    name: "Joseph Ducreux",
+    name:     "Joseph Ducreux",
     username: "jd1735",
     location: "Paris, France",
-    avatar: "images/avatar-ducreux.jpg",
-    post: "images/post-ducreux.jpg",
-    comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-    likes: 152
+    avatar:   "/images/avatar-ducreux.jpg",
+    post:     "/images/post-ducreux.jpg",
+    comment:  "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+    likes:    152
   }
 ];
 
@@ -37,8 +37,8 @@ function renderPosts() {
 
     article.innerHTML = `
       <header class="post-header">
-        <img src="${post.avatar}" 
-             alt="${post.name}’s avatar" 
+        <img src="${post.avatar}"
+             alt="${post.name}’s avatar"
              class="avatar">
         <div class="user-info">
           <h2 class="username">${post.name}</h2>
@@ -46,19 +46,19 @@ function renderPosts() {
         </div>
       </header>
       <figure class="post-image">
-        <img src="${post.post}" 
+        <img src="${post.post}"
              alt="Artwork by ${post.username}">
       </figure>
       <footer class="post-footer">
         <nav class="actions">
-          <img src="images/icon-heart.png"   
-               alt="Like"    
+          <img src="/images/icon-heart.png"
+               alt="Like"
                class="icon like">
-          <img src="images/icon-comment.png" 
-               alt="Comment" 
+          <img src="/images/icon-comment.png"
+               alt="Comment"
                class="icon comment">
-          <img src="images/icon-dm.png"      
-               alt="Direct Message" 
+          <img src="/images/icon-dm.png"
+               alt="Direct Message"
                class="icon dm">
         </nav>
         <p class="likes">${post.likes.toLocaleString()} likes</p>
@@ -73,27 +73,23 @@ function renderPosts() {
     const likeIcon = article.querySelector(".icon.like");
     const likesEl  = article.querySelector(".likes");
 
+    // initialize liked state
     post._liked = post._liked || false;
-
-    likeIcon.src = post._liked 
-      ? "images/icon-heart-filled.png" 
-      : "images/icon-heart.png";
-
-    likeIcon.addEventListener("click", () => {
-    post._liked = !post._liked;
-
-    post.likes += post._liked ? 1 : -1;
-    likesEl.textContent = `${post.likes.toLocaleString()} likes`;
-
     likeIcon.classList.toggle("liked", post._liked);
-  });
 
+    // click to toggle like
+    likeIcon.addEventListener("click", () => {
+      post._liked = !post._liked;
+      post.likes += post._liked ? 1 : -1;
+      likesEl.textContent = `${post.likes.toLocaleString()} likes`;
+      likeIcon.classList.toggle("liked", post._liked);
+    });
+
+    // dblclick image to like
     const imgEl = article.querySelector(".post-image img");
     imgEl.addEventListener("dblclick", () => {
-    if (!post._liked) {
-      likeIcon.click(); 
-    }
-});
+      if (!post._liked) likeIcon.click();
+    });
   });
 }
 
